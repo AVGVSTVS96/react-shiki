bb640f493e70a06ef9df2b6cbee2553c9d0730e0
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2024-08-04 01:00:02 -0400

    chore(config): Add `tsup`, update `tsconfig` and `package.json`

---
+++ package/tsup.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig((options) => {
+  const dev = !!options.watch;
+  return {
+    entry: ['src/index.ts'],
+    format: ['esm'],
+    
+    target: 'es2022',
+    dts: true,
+    sourcemap: true,
+    clean: true,
+    minify: !dev,
+    tsconfig: 'tsconfig.json',
+  };
+});
