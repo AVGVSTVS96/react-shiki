afda0805b5bbe9d8da1d3c32a106ed36e69a08e3
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2024-08-05 00:52:15 -0400

    chore: Update demo page with README info
    Update themes for each code block

--- playground/src/CodeTest.tsx
+++ playground/src/CodeTest.tsx
@@ -119,6 +119,82 @@ function Houston() {
         </li>
       </ul>
       <Houston />
+      <p>
+        It can also be used with <code>react-markdown</code>
+      </p>
+      <ShikiHighlighter language="jsx" theme="vitesse-black">
+        {`import type { ReactNode } from 'react';
+import type { BundledLanguage } from 'shiki';
+import ShikiHighlighter, { isInlineCode, type Element } from 'react-shiki';
+
+interface CodeHighlightProps {
+  className?: string | undefined;
+  children?: ReactNode | undefined;
+  node?: Element | undefined;
+}
+
+export const CodeHighlight = ({
+  className,
+  children,
+  node,
+  ...props
+}: CodeHighlightProps): JSX.Element => {
+  const match = className?.match(/language-(\w+)/);
+  // TODO: remove need for consumer use of BundledLanguage from shiki
+  const language = match ? (match[1] as BundledLanguage) : undefined;
+
+  const isInline: boolean | undefined = node ? isInlineCode(node) : undefined;
+
+  return !isInline ? (
+    <ShikiHighlighter
+      language={language as BundledLanguage}
+      theme={'houston'}
+      {...props}>
+      {String(children)}
+    </ShikiHighlighter>
+  ) : (
+    <code className={className} {...props}>
+      {children}
+    </code>
+  );
+};`}
+      </ShikiHighlighter>
+      <p>
+        Pass CodeHighlight to <code>react-markdown</code> as a code
+        component
+      </p>
+      <ShikiHighlighter language="jsx" theme="rose-pine-moon">
+        {`import ReactMarkdown from 'react-markdown';
+import { CodeHighlight } from './CodeHighlight';
+
+<ReactMarkdown components={{ code: CodeHighlight }}>
+  {markdown}
+</ReactMarkdown>`}
+      </ShikiHighlighter>
+      <p>
+        This works great for highlighting in realtime on the client, I use
+        it for an LLM chatbot UI, it renders markdown and highlights code
+        in memoized chat messages:
+      </p>
+      <ShikiHighlighter language="jsx" theme="material-theme-ocean">
+        {`const RenderedMessage = React.memo(({ message }: { message: Message }) => (
+  <div className={cn(messageStyles[message.role])}>
+    <ReactMarkdown components={{ code: CodeHighlight }}>
+      {message.content}
+    </ReactMarkdown>
+  </div>
+));
+
+export const ChatMessages = ({ messages }: { messages: Message[] }) => {
+  return (
+    <div className='space-y-4'>
+      {messages.map((message) => (
+        <RenderedMessage key={message.id} message={message} />
+      ))}
+    </div>
+  );
+};`}
+      </ShikiHighlighter>
     </div>
   );
 });
