586563b249366ce5a4299a8adfd1e04f2f18cf5b
AVGVSTVS96 <bassim101@gmail.com>
2024-08-07 13:40:06 -0400

    docs: Add jsdoc to useShikiHighlighter hook

--- package/src/useShiki.ts
+++ package/src/useShiki.ts
@@ -3,11 +3,21 @@ import { type ReactNode, useEffect, useState } from 'react';
 import { type BundledLanguage, type BundledTheme, codeToHtml } from 'shiki';
 import { removeTabIndexFromPre } from '@/utils';
 
+/**
+ * 
+ * `useShikiHighlighter` is a custom hook that takes in the code to be highlighted, the language, and the theme, and returns the highlighted code parsed as a ReactNode.
+ *
+ * @example
+ * ```tsx
+ * const highlightedCode = useShikiHighlighter(code, language, theme);
+ * ```
+ * 
+ */
 export const useShikiHighlighter = (
   code: string,
   lang: BundledLanguage | undefined,
   theme: BundledTheme
-) => {
+): ReactNode | null => {
   const [highlightedCode, setHighlightedCode] = useState<ReactNode | null>(
     null
   );
@@ -18,14 +28,14 @@ export const useShikiHighlighter = (
         const html = await codeToHtml(code, {
           lang: lang as BundledLanguage,
           theme,
-            transformers: [removeTabIndexFromPre],
+          transformers: [removeTabIndexFromPre],
         });
         setHighlightedCode(parse(html));
       } catch (error) {
         const fallbackHtml = await codeToHtml(code, {
           lang: 'plaintext',
           theme,
-            transformers: [removeTabIndexFromPre],
+          transformers: [removeTabIndexFromPre],
         });
         setHighlightedCode(parse(fallbackHtml));
       }
