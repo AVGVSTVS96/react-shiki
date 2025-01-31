3a2e4001d7d1b538ddf2851afcc37a4ff7d8dbfa
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2025-01-31 01:10:00 -0500

    fix: Update type for and improve Shiki transformer
    Use correct `ShikiTransformer` type and update `removeTabIndexFromPre`
    to safely access `'properties' in node`

--- package/src/utils.ts
+++ package/src/utils.ts
@@ -1,13 +1,18 @@
 import type { Element } from 'hast';
+import type { ShikiTransformer } from 'shiki';
 
 export const isInlineCode = (node: Element): boolean => {
   return node.position?.start.line === node.position?.end.line;
 };
 
 
-export const removeTabIndexFromPre = {
-  pre(node: Element) {
-    node.properties.tabindex = '-1';
+
+export const removeTabIndexFromPre: ShikiTransformer = {
+  pre(node) {
+    if ('properties' in node) {
+      node.properties.tabindex = '-1';
+    }
+    return node;
   },
 };
 
