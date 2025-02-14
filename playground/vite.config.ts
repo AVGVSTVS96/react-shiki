e7a7c0912f1ff575b930e2b2402a10ba9aaab98d
Bassim Shahidy <122117267+AVGVSTVS96@users.noreply.github.com>
2025-02-14 00:32:20 -0500

    feat(demo): Add MDX and update demo (#17)

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 // https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import mdx from '@mdx-js/rollup';
+import tailwind from '@tailwindcss/vite';
+
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(), mdx(), tailwind()],
 });
+
