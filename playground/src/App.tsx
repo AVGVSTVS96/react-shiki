0577999bf8d44fa8e4dde74b9584b98d99ab0671
Bassim Shahidy <bassim101@gmail.com>
2025-02-25 13:03:03 -0500

    fix(playground): Demo site light mode, closes #28
    Fixes wrong colors when in light mode, ensures dark styles are only
    applied when prefers-color-scheme is dark

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -3,9 +3,9 @@ import Demo from './Demo.mdx';
 function App() {
   return (
     <main className='flex flex-col min-w-screen'>
-      <div className='flex flex-col prose prose-invert self-center mt-10'>
+      <div className='flex flex-col prose dark:prose-invert self-center mt-10'>
         <Demo />
-        <footer className='text-sm text-slate-300 mt-10 mb-8 self-end'>
+        <footer className='text-sm dark:text-slate-300 mt-10 mb-8 self-end'>
           Made with ❤️ by Bassim -{' '}
           <a href="https://github.com/avgvstvs96">
             @avgvstvs96
